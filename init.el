fe224a2ec903e855ef6bd25f70f24d72061a475d
tohan.marchand@protonmail.com <dEb1An GnU/hUrD>
2021-06-29 09:50:07 -0400

    Appended bookmarks Added all-the-icons, doom-modeline, doom-themes and yasnippets

--- init.el
+++ init.el
@@ -14,10 +14,11 @@
  '(c-mode-common-hook '(c-toggle-auto-hungry-state subword-mode))
  '(c-mode-hook nil)
  '(column-number-mode t)
- '(custom-enabled-themes '(gruvbox-dark-medium))
+ '(custom-enabled-themes '(doom-gruvbox))
  '(custom-safe-themes
-   '("83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "6bdcff29f32f85a2d99f48377d6bfa362768e86189656f63adbf715ac5c1340b" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
+   '("5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "6b1abd26f3e38be1823bd151a96117b288062c6cde5253823539c6926c3bb178" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "e2c926ced58e48afc87f4415af9b7f7b58e62ec792659fcb626e8cba674d2065" "f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "f7fed1aadf1967523c120c4c82ea48442a51ac65074ba544a5aefc5af490893b" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "6bdcff29f32f85a2d99f48377d6bfa362768e86189656f63adbf715ac5c1340b" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(display-line-numbers 'visual)
+ '(font-use-system-font nil)
  '(fringe-mode '(1 . 1) nil (fringe))
  '(ido-mode 'both nil (ido))
  '(indicate-buffer-boundaries nil)
@@ -25,8 +26,9 @@
  '(initial-buffer-choice t)
  '(lisp-mode-hook '(slime-lisp-mode-hook))
  '(package-selected-packages
-   '(markdown-mode csv-mode auto-complete org-bullets slime flymake gruvbox-theme info-colors focus flymake-vala vala-mode csharp-mode))
+   '(doom-themes doom-modeline yasnippet markdown-mode csv-mode auto-complete org-bullets slime flymake gruvbox-theme info-colors focus flymake-vala vala-mode csharp-mode))
  '(pdf-view-midnight-colors '("#fdf4c1" . "#282828"))
+ '(rcirc-server-alist '(("irc.libera.chat" :nick "spowmtom" :channels nil)))
  '(scroll-bar-mode nil)
  '(size-indication-mode t)
  '(sml/line-number-format "%3l")
@@ -69,6 +71,8 @@
 ;; (package-initialize)
 
 (require 'writer)
+(require 'doom-modeline)
+(doom-modeline-mode 1)
 
 
 ;;; Functions
@@ -85,8 +89,7 @@
   "Inserts the lambda (λ) character.
 ARG defaults to 1"
   (interactive "p")
-  (or arg (setq arg 2))
-  (dotimes (i arg)
+  (dotimes (i (cond (arg arg) 1))
     (insert "λ")))
 
 
